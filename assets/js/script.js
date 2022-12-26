a513126b07a3ed29b3d763cc53d0442069fc6994
MLD4 <dongmiao123@gmail.com>
2022-12-26 20:40:23 +1030

    storage var and empty functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var Lower  = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var upper = 'abcdefghijklmnopqrstuvwxyz'.split('');
+var number = '123456790'.split('');
+var special = '!@#$%^&*()-_=+/"'.split('');
+
+var pwLength;
+var lowerStore;
+var upperStore;
+var numberStore;
+var specialStore;
+
+// user input and storing of those imputs
+function promptPassword() {
+
+}
+// take user input and makes a string of characters
+function gereratePassword() {
+
+}
+
+function writePassword() {
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +31,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
